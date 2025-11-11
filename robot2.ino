bfe18bc5210211a77c7b3da8ed0989811b3dd875
ianalfaaaaaaaa <ian.floreano@studenti.malignani.ud.it>
2025-11-11 09:42:51 +0100

    Update robot2.ino

--- robot2.ino
+++ robot2.ino
@@ -12,9 +12,9 @@ DATA INIZIO: 28 ott 2025
 
 const int antSx=1700, antDx=1685, orarSx=1300, orarDx=1360, fermo=1500;	//definizione sensi di rotazione delle ruote
 
-const int pinSx=12,pinDx=13;													//definizione pin dei servomotori
+const int pinSx=12,pinDx=13;													                  //definizione pin dei servomotori
 
-const int sogliaSensore = 400;
+const int sogliaSensore = 400;                                          //Soglia lettura analogica oltre la quale il valore è considerato 1
 
 Servo servoRight;
 Servo servoLeft;
@@ -23,18 +23,12 @@ void setup() {
 
   Serial.begin(9600);
 
-  pinMode(5, INPUT);
-  pinMode(4, INPUT);
-  pinMode(3, INPUT);
-  
-
   servoRight.attach(pinDx);
   servoLeft.attach(pinSx);
 
   servoRight.writeMicroseconds(fermo); 
   servoLeft.writeMicroseconds(fermo); 
   delay(3000);
-
 
 }
 
@@ -52,21 +46,20 @@ void loop() {
   if(dx  > sogliaSensore) dx =  1; else dx  = 0;
   if(ce  > sogliaSensore) ce =  1; else ce  = 0;
 
-  if((sx==1 && ce==0 && dx==1)|| (sx==0 && ce==0 && dx==0 )){
-    forward();
-  }
-   if((sx==1 && ce==0 && dx==0) || (sx==1 && ce==1 && dx==0) || (sx==0 && ce==0 && dx==0 && dx2==0 && sx2==1)){
-    turnright();
-  }
-  if((sx==0 && ce==0 && dx==1) || (sx==0 && ce==1 && dx==1) || (sx==0 && ce==0 && dx==0 && dx2==1 && sx2==0)){
-    turnleft();
-  }
+  if((sx==1 && ce==0 && dx==1)|| (sx==0 && ce==0 && dx==0 )) forward();
+
+   if((sx==1 && ce==0 && dx==0) || (sx==1 && ce==1 && dx==0) || (sx==0 && ce==0 && dx==0 && dx2==0 && sx2==1)) turnright();
+  
+  if((sx==0 && ce==0 && dx==1) || (sx==0 && ce==1 && dx==1) || (sx==0 && ce==0 && dx==0 && dx2==1 && sx2==0)) turnleft();
+  
   if(sx==0 && ce==0 && dx==0 && dx2==0 && sx2==0){
     servoRight.detach();
     servoLeft.detach();
   }
-
+  
 }
+
+//funzioni di movimento
 void forward(){
   servoRight.writeMicroseconds(orarDx); 
   servoLeft.writeMicroseconds(antSx); 
